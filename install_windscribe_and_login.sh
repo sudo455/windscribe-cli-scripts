6ce3ec180c8e90946050a51276b45d3f63c13c13
sudo455 <aggelosmoraitis1@gmail.com>
2022-12-12 15:47:35 +0200

    fix some bugs and spacing in install windscribeand login script

--- install_windscribe_and_login.sh
+++ install_windscribe_and_login.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-echo "clone and installing windscribe-cli"
+
+echo ""
+echo "github clone and installing windscribe-cli"
+echo ""
 git clone https://github.com/hkuchampudi/Windscribe.git
 cd Windscribe/
 makepkg -si
+
 # cloning and installing windscribe-cli
-echo "\n done"
+echo ""
+echo "done"
+echo ""
+
 # starting the service
-echo "Status of the winscribe-cli systemd service: \n"
+echo "Status of the winscribe-cli systemd service:"
+echo ""
 systemctl status windscribe.service
-echo "Starting windscribe-cli: \n"
+echo ""
+echo "Starting windscribe-cli:"
+echo ""
 sudo systemctl start windscribe
-echo "Enabling windscribe-cli systemd service \n"
+echo ""
+echo "Enabling windscribe-cli systemd service"
+echo ""
 sudo systemctl enable windscribe
 
 #login part
+echo ""
+echo "now you will be prompted to login with your credentials"
+echo ""
 windscribe login
 
 # and removing the folder from the working tree
